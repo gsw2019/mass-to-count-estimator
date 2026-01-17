6103546611e93216a081b7c0c64c9e6c6493904b
gsw2019 <wsg@arizona.edu>
2026-01-17 16:24:36 -0700

    updated backend main.py server port for DigitalOcean

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,5 +2,5 @@ import uvicorn
 
 if __name__ == "__main__":
     # entry point = app.api:app
-    # uvicorn server run at localhost:8000
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn server run at 0.0.0.0:8080
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8080, reload=True)
